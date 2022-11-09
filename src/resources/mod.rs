8faf0314d4b6017790429e42d0eee29016ac6db8
Pavel Pscheidl <pavel2@pscheidl.cz>
2022-11-09 07:32:47 +0100

    build: Kube 0.76

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,5 +1,6 @@
 use k8s_openapi::serde::__private::fmt::Debug;
 use k8s_openapi::serde::de::DeserializeOwned;
+use k8s_openapi::NamespaceResourceScope;
 use kube::api::ListParams;
 use kube::{Api, Client, Resource};
 
@@ -9,6 +10,7 @@ pub async fn list_resource<T>(client: &Client, namespace: &str) -> Result<Vec<T>
 where
     T: Clone + Debug + DeserializeOwned + Resource,
     <T as Resource>::DynamicType: Default,
+    T: kube::Resource<Scope = NamespaceResourceScope>,
 {
     let resource_api = Api::<T>::namespaced(client.clone(), namespace);
     Ok(resource_api.list(&ListParams::default()).await?.items)
