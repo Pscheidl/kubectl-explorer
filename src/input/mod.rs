7bbc7606a0c8f1af0b44672253ae2b648c9fc471
Pavel Pscheidl <pavel2@pscheidl.cz>
2023-04-11 10:03:00 +0200

    refactor: Clap 4

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,45 +1,51 @@
-use clap::{App, Arg};
+use clap::{builder::PossibleValuesParser, Arg, ArgAction, Command};
 use std::str::FromStr;
 
 pub fn parse_user_input() -> UserArgs {
-    let matches = App::new("KubEx - Kubernetes Explorer")
+    let matches = Command::new("KubEx - Kubernetes Explorer")
         .version("0.1.0")
         .author("Pavel Pscheidl <pavelpscheidl@gmail.com>")
         .about("Discovers unused ConfigMaps and Secrets")
         .arg(
-            Arg::with_name("KUBECONFIG")
+            Arg::new("KUBECONFIG")
                 .short('k')
                 .long("kubeconfig")
                 .value_name("PATH_TO_KUBECONFIG")
                 .help("Path to a KUBECONFIG file. When not set, env is used.")
-                .takes_value(true),
+                .action(ArgAction::Set),
         )
         .arg(
-            Arg::with_name("NAMESPACE")
+            Arg::new("NAMESPACE")
                 .short('n')
                 .long("namespace")
                 .value_name("NAMESPACE")
                 .help("Namespace to search in.")
-                .takes_value(true),
+                .action(ArgAction::Set),
         )
         .arg(
-            Arg::with_name("OUTPUT")
+            Arg::new("OUTPUT")
                 .short('o')
                 .long("output")
                 .value_name("OUTPUT")
                 .help("Output format. YAML by default.")
-                .possible_values(["yaml", "json"])
+                .value_parser(PossibleValuesParser::new(["yaml", "json"]))
                 .default_value("yaml")
-                .takes_value(true),
+                .action(ArgAction::Set),
         )
         .get_matches();
 
     UserArgs::new(
-        matches.value_of("KUBECONFIG").map(|arg| arg.to_string()),
-        matches.value_of("NAMESPACE").map(|arg| arg.to_string()),
-        matches.value_of("OUTPUT").map_or(Output::Yaml, |arg| {
-            Output::from_str(arg).unwrap_or(Output::Yaml)
-        }),
+        matches
+            .get_one::<String>("KUBECONFIG")
+            .map(|arg| arg.to_string()),
+        matches
+            .get_one::<String>("NAMESPACE")
+            .map(|arg| arg.to_string()),
+        matches
+            .get_one::<String>("OUTPUT")
+            .map_or(Output::Yaml, |arg| {
+                Output::from_str(arg).unwrap_or(Output::Yaml)
+            }),
     )
 }
 
